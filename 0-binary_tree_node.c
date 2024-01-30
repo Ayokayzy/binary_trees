8fef2486a4b5bdd2ee6afeeae604022a4a53e907
Ayokayzy <theayokayzy1@gmail.com>
2024-01-30 14:12:32 +0000

    New node

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -11,6 +11,8 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *node;
 
+	if (value == NULL)
+		return (NULL);
 	node = malloc(sizeof(binary_tree_t));
 	node->parent = parent;
 	node->n = value;

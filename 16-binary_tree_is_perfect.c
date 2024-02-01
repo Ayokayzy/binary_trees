85c9eabceaac63964469a3ce711bb395235691b9
Ayokayzy <theayokayzy1@gmail.com>
2024-02-01 11:15:04 +0000

    perfect

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -14,8 +14,8 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
-	if (tree->left == NULL || tree->right == NULL)
-		return (0);
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
 
 	l_height = binary_tree_height(tree->left);
 	r_height = binary_tree_height(tree->right);

5a44ac34cbb3e9671a550b200dd93d972abd9460
Ayokayzy <theayokayzy1@gmail.com>
2024-02-02 10:15:41 +0000

    Lowest common ancestor

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -9,7 +9,9 @@
  * If no common ancestor was found, your function must return NULL
  */
 
-binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second)
+binary_tree_t *binary_trees_ancestor(
+	const binary_tree_t *first, const binary_tree_t *second
+)
 {
 	if (first == second->parent)
 		return ((binary_tree_t *) first);

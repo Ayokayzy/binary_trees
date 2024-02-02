05be99e4e5683f4645b280c48f27fdd2860d1374
Ayokayzy <theayokayzy1@gmail.com>
2024-02-02 10:06:35 +0000

    Lowest common ancestor

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -11,13 +11,13 @@
 
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second)
 {
+	if (first == second->parent)
+		return ((binary_tree_t *) first);
 	while (first->parent)
 	{
 		while (second->parent)
 		{
-			if (first == second->parent)
-				return ((binary_tree_t *) first);
-			else if (first->parent == second->parent)
+			if (first->parent == second->parent)
 				return (first->parent);
 			second = second->parent;
 		}
